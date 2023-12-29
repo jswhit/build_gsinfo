63ca2e56f434cca66b883663ab56bedd31aed3ca
Jeffrey.S.Whitaker <Jeffrey.S.Whitaker@noaa.gov>
2023-12-29 16:00:03 -0500

    script to check for new sat instrument/channel

---
+++ checknewsat.sh
@@ -0,0 +1,30 @@
+# create global_satinfo file for a given date
+date=$1
+# loop over satellites
+cd satinfo
+newsats=0
+for sat in $(cat satellites); do
+    # find matching date, check for use flag=1
+    newsat=0
+    for f in $sat/*; do
+        if [ $f != "$sat/readme" ]; then # skip readme file
+           datex=`basename $f`
+	   if [ $date -eq $datex ]; then
+              useflags=`awk '{print $3}' $f`
+	      for flag in $useflags; do
+                 if [ $flag -eq 1 ]; then
+                    newsat=1
+		    ((newsats+=1))
+                 fi
+              done
+           fi
+        fi
+    done
+    if [ $newsat -eq 1 ]; then
+       echo "$sat is new"
+    fi
+done
+if [ $newsats -eq 0 ]; then
+   echo "no new sats"
+fi
+exit $newsats
